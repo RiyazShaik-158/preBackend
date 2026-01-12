5d963103efb4e3a6a0f998e55aae6f6954720be0
RiyazShaik-158 <riyaz.shaik.1481@gmail.com>
2026-01-12 21:38:53 +0530

    feat(new function): initialized new function with multiple params

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -18,9 +18,18 @@ public class HelloWorld {
         System.out.println(s + " is the given string param");
     }
 
+    public static void calculateCostOfMeal(double price, double tipRate, double taxRate) {
+        double tip = tipRate * price;
+        double tax = taxRate * price;
+        double totalPrice = tip + tax + price;
+        System.out.println("Your total meal pirce is " + totalPrice);
+    }
+
+
     public static void main(String[] args) {
         System.out.println(runIfElse(10));
         runNewFunction();
         runNewFunctionWithParameters("Tea Time");
+        calculateCostOfMeal(20, 0.1, 0.8);
     }
 }
