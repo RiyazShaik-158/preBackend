3084f0c3c57e767612503a61522e556d98f564a8
RiyazShaik-158 <riyaz.shaik.1481@gmail.com>
2026-01-15 10:14:09 +0530

    feat(new class): triangle class second instance

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
         Triangle rigthAngled = new Triangle(3,4,3,4,5); //instance
+        Triangle rigthAngledAnother = new Triangle(4,3,3,4,5);
 
         double Area1 = rigthAngled.findArea(); //instance method
+        double Area2 = rigthAngledAnother.findArea();
 
         System.out.println("Area of first triangle is " + Area1);
+        System.out.println("Area of second triangle is " + Area2);
     }
 }
